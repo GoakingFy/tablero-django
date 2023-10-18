b9f86f0774d6dfa9cfd3893ea98e8db902952070
GoakingFy <joaquindiaz66@gmail.com>
2023-10-19 01:07:56 +0200

    se ha agregado codigo

--- buscaminas/models.py
+++ buscaminas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+   
+    rows = models.CharField(max_length=200)
+    columns = models.TextField()
+    
--- buscaminas/views.py
+++ buscaminas/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .forms import PostForm
 
 # Create your views here.
 
 def indexpage(request):
     return render(request, 'buscaminas/index.html', {})
+
+def table_new(request):
+    form = PostForm()
+    return render(request, 'buscaminas/index.html', {'form': form})
